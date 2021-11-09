a29a4df44bef9c8a42442e525627100139252467
Umratul <umratul0904@gmail.com>
2021-11-09 23:47:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u2jer.mongodb.net/myFirstDatabase?retryWrites=true&w=majority1`;
+
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('connected');
+        const database = client.db('country');
+        const servicesCollection = database.collection('services');
+
+        //get
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // get single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        });
+
+        // post
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the api',service);
+            
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+
+        });
+
+        //delete
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+       // await client.close(); 
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Server');
+});
+
+app.listen(port, () => {
+    console.log('Running Server on port', port);
+})
